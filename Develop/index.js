d5cf49e64373f06f4800f2ffb711d35dd7c25e2a
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-03-07 19:01:20 -0800

    Updated some index.js and markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,87 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// questions section for user input:
+inquirer
+    .prompt([
+
+        {
+            type: "input",
+            message: 'What is the title of the Project?',
+            name: 'title',
+        },
+
+        {
+            type: "input",
+            message: 'Enter a description for the project',
+            name: 'description',
+        },
+
+        {
+            type: "input",
+            message: 'What installation instructions are needed for project?',
+            name: 'installation',
+        },
+
+        {
+            type: "input",
+            message: 'What is the app used for?',
+            name: 'usage',
+        },
+
+        {
+            type: "input",
+            message: 'What are the tests required?',
+            name: 'test',
+        },
+
+        {
+            type: "input",
+            message: 'Contributor(s) GitHub accounts associated with this project?',
+            name: 'contributors',
+        },
+
+        {
+            type: "list",
+            message: 'What license will be used?',
+            name: 'license',
+            choices:['']
+        },
+
+
+        {
+            type: "input",
+            message: 'What is your GitHub username?',
+            name: 'username',
+        },
+
+        {
+            type: "input",
+            message: 'What is your email?',
+            name: 'email',
+        },
+    ])
+    .then((answers) => {
+        // Use user feedback for... whatever!!
+        
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
-init();
+init(generateMarkdown);
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# 
+  
+  ${data.title}
+  ## Description
+  ${data.description}
+
+  ##Table of Contents
+  -[Installation](##Installation)
+  -[Test-Required](##Test-Required)
+  -[Contributors](##Contributors)
+  -[Contact-Info](##Contact-Info)
+
+  ## Installation
+  ${data.installation}
+
+  ## Data Usage
+  ${data.usage}
+
+  ## Tests Required
+  ${data.test}
+
+  #Contributors
+  ${data.contributors}
+
+  ##License Used
+  ${data.license}
+
+  # Contact Info
+  Have questions? Please reach me at ${data.email}
 
 `;
 }
